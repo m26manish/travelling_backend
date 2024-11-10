bb2176e6483134a261b8cae3ea5ad3971127ea50
manish yadav <manishyadav@manishs-Laptop.local>
2024-11-10 16:07:58 +0530

    fix cors origin

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use( cors());
+app.use(cors({
+  origin: '*'
+}));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
